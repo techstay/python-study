8e54d218fce8b13d5c2d242664971823b974331f
yitian <techstay@aliyun.com>
2017-04-30 18:57:33 +0800

    添加了第二章的答案

---
+++ introducing-python-answers/chapter2/__init__.py
@@ -0,0 +1,21 @@
+# Q1
+seconds_per_minute = 60
+minutes_per_hour = 60
+
+seconds_per_minute * minutes_per_hour
+
+# Q2
+seconds_per_hour = seconds_per_minute * minutes_per_hour
+
+# Q3
+hours_per_day = 24
+hours_per_day * seconds_per_hour
+
+# Q4
+seconds_per_day = hours_per_day * seconds_per_hour
+
+# Q5
+seconds_per_day / seconds_per_hour
+
+# Q6
+seconds_per_day // seconds_per_hour
